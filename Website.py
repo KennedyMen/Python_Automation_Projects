279b78fad9a65d5c204e1f2d4be684e6d78a7dd1
KennedyMen03032731 <152940711+KennedyMen03032731@users.noreply.github.com>
2024-05-18 07:33:19 -0400

    all messed up have to make changes to how the data is collected will come back tommorow

--- Website.py
+++ Website.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 website = "https://www.the-sun.com/entertainment/11399168/mary-kate-ashley-olsen-reunite-full-house-cast/"
@@ -9,9 +11,12 @@ path = "/Users/033103kennedymensah/Downloads/STASH_ON_DEVICE/chromedriver-mac-ar
 
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
+
 driver.get(website)
+driver.implicitly_wait(10)
 containers = driver.find_elements(
     by="xpath", value='//div[@class="article-recommendation-container"]')
+print(len(containers))
 titles = []
 subtitles = []
 links = []
@@ -23,14 +28,14 @@ for container in containers:
     if size < 5:
         Title = container.find_element(
             by="xpath", value='//div[@class="article-recommendation-container"]/a/div/div/div/div/span').text
+        titles.append(Title)
     else:
         Subtitle = container.find_element(
             by="xpath", value='//div[@class="article-recommendation-container"]/a/div/div/div/div/span').text
+        subtitles.append(Subtitle)
 # //div[@class="article-recommendation-container"]/a
-    link = container.find_enlement(
-        by="xpath", value='//div[@class="article-recommendation-container"]/a').get_atttribute('href')
-    titles.append(Title)
-    subtitles.append(Subtitle)
+    link = container.find_element(
+        by="xpath", value='//div[@class="article-recommendation-container"]/a').get_attribute('href')
     links.append(link)
 
 dictionary = ({'Title': titles, 'Subtitle': subtitles, 'Link': links})
