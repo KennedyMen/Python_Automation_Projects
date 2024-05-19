fa4ca31e34ac2ff156dbe66a6edd17fa341fd58b
KennedyMen03032731 <152940711+KennedyMen03032731@users.noreply.github.com>
2024-05-18 22:57:08 -0400

    Just did the same for the PDF_Reading.py making sure it stays in the directory if you clone it

--- PDF/PDF_Reading.py
+++ PDF/PDF_Reading.py
@@ -1,8 +1,11 @@
 import camelot
+from pathlib import Path
 
 
 tables = camelot.read_pdf('foo.pdf', pages='1',)
 print(tables)
 
 tables.export('foo.csv', f='csv', compress=True)  # json, excel, html
-tables[0].to_csv('foo.csv')  # to_json, to_excel, to_html
+filepath = Path('/Users/033103kennedymensah/Automation/PDF/foo.csv')
+filepath.parent.mkdir(parents=True, exist_ok=True)
+tables[0].to_csv(filepath)  # to_json, to_excel, to_html
